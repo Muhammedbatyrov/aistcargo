d1c25840576c2c5e95bfcfed2270aedbd6d623d6
Muhammedbatyrov <jstjustt@gmail.com>
2024-12-09 18:45:36 +0600

    c

---
+++ src/main/java/com/aistcargo/AistcargoApplication.java
@@ -0,0 +1,13 @@
+package com.aistcargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AistcargoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AistcargoApplication.class, args);
+    }
+
+}
