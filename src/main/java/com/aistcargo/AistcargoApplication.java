38de652380a15b1b414159a5306f043294bbe66b
Akzholbek Abdikarim uulu <akzholbek0096@gmail.com>
2024-12-13 17:03:02 +0000

    test

--- src/main/java/com/aistcargo/AistcargoApplication.java
+++ src/main/java/com/aistcargo/AistcargoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AistcargoApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(AistcargoApplication.class, args);
     }
